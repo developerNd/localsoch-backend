fe40b941a26085f3a84583ccb4d4c9ff7809c9d7
developerNd <91623104+developerNd@users.noreply.github.com>
2025-08-18 00:51:19 +0530

    featured updated

--- src/api/subscription-plan/routes/subscription-plan.js
+++ src/api/subscription-plan/routes/subscription-plan.js
@@ -2,40 +2,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-const defaultRouter = createCoreRouter('api::subscription-plan.subscription-plan');
-
-const customRouter = (innerRouter, extraRoutes = []) => {
-  let routes;
-  return {
-    get prefix() {
-      return innerRouter.prefix;
-    },
-    get routes() {
-      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
-      return routes;
-    },
-  };
-};
-
-const myExtraRoutes = [
-  {
-    method: 'GET',
-    path: '/subscription-plans/active',
-    handler: 'subscription-plan.getActivePlans',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-  {
-    method: 'GET',
-    path: '/subscription-plans/popular',
-    handler: 'subscription-plan.getPopularPlans',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-];
-
-module.exports = customRouter(defaultRouter, myExtraRoutes); 
+module.exports = createCoreRouter('api::subscription-plan.subscription-plan'); 
